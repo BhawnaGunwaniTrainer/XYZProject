8e8757443084a5e7894809285c1344f6c2da21d2
BhawnaGunwaniTrainer <bhawna.gunwani.trainer@gmail.com>
2019-03-04 09:19:53 -0800

    Add project files.

---
+++ XYZProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace XYZProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
